a7f4f440634eaf7054ae690b4cf22f4202d95bcc
Robin Salkeld <salkeldr@amazon.com>
2024-10-29 07:40:11 -0700

    —stacktrace

--- SmithyDafnyMakefile.mk
+++ SmithyDafnyMakefile.mk
@@ -550,7 +550,7 @@ mvn_staging_deploy:
 
 test_java:
 	$(GRADLEW) -p runtimes/java runTests
-	$(GRADLEW) -p runtimes/java test --info
+	$(GRADLEW) -p runtimes/java test --info --stacktrace
 
 ########################## Rust targets
 
