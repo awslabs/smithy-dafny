2580e984e7ca161637f04d82bb61473b1313cf6a
Andy Jewell <ajewell@amazon.com>
2024-11-03 11:50:49 -0500

    m

--- codegen/smithy-dafny-codegen/src/test/java/software/amazon/polymorph/smithyjava/generator/awssdk/v2/ToNativeTest.java
+++ codegen/smithy-dafny-codegen/src/test/java/software/amazon/polymorph/smithyjava/generator/awssdk/v2/ToNativeTest.java
@@ -354,6 +354,11 @@ public class ToNativeTest extends ForEachDafnyTest {
     final Path actualPath = actual.keySet().toArray(temp)[0];
     assertEquals(expectedPath, actualPath);
     final String actualSource = actual.get(actualPath).toString();
+    System.err.println("*******");
+    System.err.println(actualSource);
+    System.err.println("*******");
+    System.err.println(ToNativeConstants.KMS_A_STRING_OPERATION_JAVA_FILE);
+    System.err.println("*******");
     tokenizeAndAssertEqual(
       ToNativeConstants.KMS_A_STRING_OPERATION_JAVA_FILE,
       actualSource
