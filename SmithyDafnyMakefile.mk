25bdb21792c5f1680e3962709c6c38a367251509
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-11-06 12:17:53 -0800

    m

--- SmithyDafnyMakefile.mk
+++ SmithyDafnyMakefile.mk
@@ -347,9 +347,7 @@ _polymorph_code_gen: OUTPUT_DOTNET=\
 _polymorph_code_gen: OUTPUT_JAVA=--output-java $(LIBRARY_ROOT)/runtimes/java/src/main/smithy-generated
 _polymorph_code_gen: OUTPUT_JAVA_TEST=--output-java-test $(LIBRARY_ROOT)/runtimes/java/src/test/smithy-generated
 _polymorph_code_gen: OUTPUT_PYTHON=--output-python $(LIBRARY_ROOT)/runtimes/python/src/$(PYTHON_MODULE_NAME)/smithygenerated
-# Defined per-Makefile
 _polymorph_code_gen: MODULE_NAME=--library-name $(PYTHON_MODULE_NAME)
-# Defined per-Makefile
 _polymorph_code_gen: DEPENDENCY_MODULE_NAMES=$(PYTHON_DEPENDENCY_MODULE_NAMES)
 _polymorph_code_gen: _polymorph
 
