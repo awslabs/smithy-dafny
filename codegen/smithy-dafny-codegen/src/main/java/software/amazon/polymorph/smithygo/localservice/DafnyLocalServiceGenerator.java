982cc9b50ce2b026b632c3cfdf95ddb8c71117a2
Shubham Chaturvedi <scchatur@amazon.com>
2024-11-06 16:18:04 -0800

    fix: local service to sdk resource error fix

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/localservice/DafnyLocalServiceGenerator.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/localservice/DafnyLocalServiceGenerator.java
@@ -831,13 +831,7 @@ public class DafnyLocalServiceGenerator implements Runnable {
                         $L
                         if (dafny_response.Is_Failure()) {
                             err := dafny_response.Dtor_error().($L.Error);
-                            ${C|}
-                            if err.Is_CollectionOfErrors() {
-                                $L CollectionOfErrors_Output_FromDafny(err)
-                            }
-                            if err.Is_Opaque() {
-                                $L OpaqueError_Output_FromDafny(err)
-                            }
+                            $L Error_FromDafny(err)
                         }
                         $L
                     }
@@ -848,27 +842,6 @@ public class DafnyLocalServiceGenerator implements Runnable {
                   outputType,
                   baseClientCall,
                   DafnyNameResolver.dafnyTypesNamespace(service),
-                  writer.consumer(w -> {
-                    for (var errorShape : model.getShapesWithTrait(
-                      ErrorTrait.class
-                    )) {
-                      w.write(
-                        """
-                                               if err.Is_$L() {
-                                               $L $L(err)
-                                           }
-                        """,
-                        errorShape.toShapeId().getName(),
-                        returnError,
-                        SmithyNameResolver.getFromDafnyMethodName(
-                          service,
-                          errorShape,
-                          ""
-                        )
-                      );
-                    }
-                  }),
-                  returnError,
                   returnError,
                   returnResponse
                 );
