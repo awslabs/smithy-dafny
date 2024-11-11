4cc65ea87cbdb361daf32622520140dcf798c073
rishav-karanjit <karanjitrishav4@gmail.com>
2024-11-11 14:27:49 -0800

    Add getDefaultValueForSmithyType

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/utils/Constants.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/utils/Constants.java
@@ -1,11 +1,31 @@
 package software.amazon.polymorph.smithygo.utils;
 
+import java.util.Map;
 import software.amazon.smithy.model.shapes.MemberShape;
 
 public class Constants {
 
   public static final String DAFNY_RUNTIME_GO_LIBRARY_MODULE =
     "github.com/dafny-lang/DafnyRuntimeGo/v4";
+  
+  private static final Map<String, String> DEFAULT_VALUES = Map.of(
+    "int32", "0",
+    "string", "",
+    "[]byte", "[0]",
+    "int64", "0",
+    "float64", "0",
+    "bool", "false"
+  );
+
+  /**
+   * Gets the default value for a specific Smithy type.
+   *
+   * @param smithyType The Smithy type to get the default value for
+   * @return The default value for the specified type, or null if the type is not found
+   */
+  public static String getDefaultValueForSmithyType(String smithyType) {
+    return DEFAULT_VALUES.getOrDefault(smithyType, "nil");
+  }
 
   // TODO: Is it possible to make this function name shorter and in camelCase?
   /**
