33d7efa56bdd3f9fb79a634ffdafc308e922cebb
Andy Jewell <ajewell@amazon.com>
2024-11-12 17:20:39 -0500

    m

---
+++ TestModels/aws-sdks/ddb-lite/runtimes/rust/src/lib.rs
@@ -0,0 +1,18 @@
+#![allow(
+    deprecated,
+    non_upper_case_globals,
+    unused,
+    non_snake_case,
+    non_camel_case_types
+)]
+
+pub mod client;
+pub mod conversions;
+pub(crate) mod implementation_from_dafny;
+mod standard_library_conversions;
+mod standard_library_externs;
+
+pub(crate) use crate::implementation_from_dafny::_Wrappers_Compile;
+pub use crate::implementation_from_dafny::software;
+pub use crate::implementation_from_dafny::UTF8;
+pub use client::Client;
