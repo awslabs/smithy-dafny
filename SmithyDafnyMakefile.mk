83cf5e2983ad6146aa21954f199f3271c3824adc
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-11-21 15:38:26 -0800

    chore(Makefile): polymorph Rust with make polymorph_code_gen

--- SmithyDafnyMakefile.mk
+++ SmithyDafnyMakefile.mk
@@ -346,6 +346,7 @@ _polymorph_code_gen: OUTPUT_DOTNET=\
     $(if $(DIR_STRUCTURE_V2), --output-dotnet $(LIBRARY_ROOT)/runtimes/net/Generated/$(SERVICE)/, --output-dotnet $(LIBRARY_ROOT)/runtimes/net/Generated/)
 _polymorph_code_gen: OUTPUT_JAVA=--output-java $(LIBRARY_ROOT)/runtimes/java/src/main/smithy-generated
 _polymorph_code_gen: OUTPUT_JAVA_TEST=--output-java-test $(LIBRARY_ROOT)/runtimes/java/src/test/smithy-generated
+_polymorph_code_gen: OUTPUT_RUST=--output-rust $(LIBRARY_ROOT)/runtimes/rust
 _polymorph_code_gen: _polymorph
 
 check_polymorph_diff:
