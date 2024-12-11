56652f9b6bceaa83b80d6950b1787f90457177ee
Robin Salkeld <salkeldr@amazon.com>
2024-12-11 14:32:43 -0800

    Another Go-disabled test

--- codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithygo/GoTestModels.java
+++ codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithygo/GoTestModels.java
@@ -34,6 +34,7 @@ class GoTestModels extends TestModelTest {
     DISABLED_TESTS.add("aws-sdks/sqs");
     DISABLED_TESTS.add("aws-sdks/sqs-via-cli");
     DISABLED_TESTS.add("aws-sdks/s3");
+    DISABLED_TESTS.add("Streaming");
     DISABLED_TESTS.add("SQSExtended");
     //TODO: We should be able to support below models, but isn't a priority.
     DISABLED_TESTS.add("MultipleModels");
