d118f2353298a5404c1d00dea1612f2bf23e4028
Robin Salkeld <salkeldr@amazon.com>
2025-01-06 13:18:13 -0800

    Revert bad merges/stale changes from python branch

--- codegen/settings.gradle.kts
+++ codegen/settings.gradle.kts
@@ -4,9 +4,6 @@
 rootProject.name = "smithy-dafny"
 include(":smithy-dafny-codegen")
 include(":smithy-dafny-codegen-cli")
-// TODO: Once Smithy-Python is published to Maven, and we do not rely on a fork, use that
-include(":smithy-python-codegen")
-project(":smithy-python-codegen").projectDir = file("./smithy-dafny-codegen-modules/smithy-python/codegen/smithy-python-codegen")
 include(":smithy-dafny-codegen-test")
 
 pluginManagement {
